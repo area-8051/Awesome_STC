6627af4c99b03c892201557a2815b1cabecd7c67
Vincent DEFERT <20.100@defert.com>
2023-04-17 15:53:47 +0200

    Cosmetic improvements

--- stc-updates.sh
+++ stc-updates.sh
@@ -33,6 +33,25 @@
 # The result is formatted in a consistent way regardless of STC
 # changing their mind about presentation from time to time.
 
+# Removes dates for which there's no URL.
+cleanDates() {
+    local l
+    local d=''
+    
+    while IFS="\n" read l; do
+        if [ "$(echo "${l}" | cut -c1-4)" = 'http' ]; then
+            if [ -n "${d}" ]; then
+                echo "${d}"
+                d=''
+            fi
+            
+            echo "${l}"
+        else
+            d="${l}"
+        fi
+    done
+}
+
 wget -q -O - http://www.stcmcudata.com/website-update.txt \
     | iconv -f GB18030 -t UTF-8 \
     | grep -e "^网站更新 " -e " 更新第.次" -e "http://" -e "^www.stcmcudata.com" \
@@ -46,8 +65,10 @@ wget -q -O - http://www.stcmcudata.com/website-update.txt \
     | sed 's/^\(20[[:digit:]]*[.-]\)\([[:digit:]][.-].*\)/\10\2/' \
     | sed 's/^\(20[[:digit:]]*\)[.-]\([[:digit:]]*\)[.-]\([[:digit:]]*\).*/\1\2\3/' \
     | sed 's/^\(20[[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)\([[:digit:]][[:digit:]]\)/\1-\2-\3/' \
+    | sed 's/^\(http:.*\)\.JPG/\1.jpg/' \
     | sed 's/^\(http:.*\)\.PNG/\1.png/' \
     | sed 's/^\(http:.*\)\.PDF/\1.pdf/' \
     | sed 's/^\(http:.*\)\.ZIP/\1.zip/' \
     | sed 's/^\(http:.*\)\.RAR/\1.rar/' \
-    | head -20
+    | head -20 \
+    | cleanDates
